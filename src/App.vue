e9b84e077dd037c661a0bfdea2caf89ed467a01a
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2022-08-02 10:14:54 +0900

    構成完了

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <template>
+  <header class="header">
+    <h1 class="title">my-portfolio</h1>
+  </header>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="my-zone">
+      <div class="name-zone">
+        <h2>園田 海斗</h2>
+        <h3>Sonoda Kaito</h3>
+      </div>
+      <div class="age-zone">
+        <h3>20歳</h3>
+      </div>
+    </div>
+    <div class="university-zone">
+      <h3>大学</h3>
+      <h4>福岡大学</h4>
+    </div>
+    <div class="course-zone">
+      <h3>学部・学科</h3>
+      <h4>工学部 電子情報工学科</h4>
+    </div>
+    <div class="sns-zone">
+      <h3>インスタグラム</h3>
+      <h4>@sonoda7777777</h4>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue"
+// import HelloWorld from "@/components/HelloWorld.vue"
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 }
 </script>
